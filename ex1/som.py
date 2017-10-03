a395aee2a7f028fdd23108e76d9aa4bb106cd844
David Söderberg <dasovm@gmail.com>
2017-10-03 11:30:39 +0200

    Added convergence phase and plots

--- ex1/som.py
+++ ex1/som.py
@@ -2,15 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 from random import randint
-from pprint import pprint
 
 INPUT_SIZE = 1000
 NEURON_SIZE = 100
 
 TAO_SIGMA = 300
-SIGMA_ZERO = 100
+SIGMA_ORDER_ZERO = 100
 T_ORDER = 1000
-ETA_ZERO = .1
+ETA_ORDER_ZERO = .1
+
+T_CONV = 20000
+SIGMA_CONV = .9
+ETA_CONV = .01
+
+PLOT_POINTS = True
+PLOT_ION = False
 
 
 def gaussian_width(sigma_zero, t, tao_sigma):
@@ -73,27 +79,67 @@ points = generate_points(INPUT_SIZE)
 np.random.shuffle(points)
 neurons_W = generate_neurons(NEURON_SIZE)
 
-plt.ion()
-plt.scatter(points[:, 0], points[:, 1], c='b')
-plt.scatter(neurons_W[:, 0], neurons_W[:, 1], c='g')
-plt.show()
+plt.figure(1)
+plt.subplot(211)
+plt.title('Ordering Phase')
+
+if PLOT_ION:
+    plt.ion()
+    plt.scatter(points[:, 0], points[:, 1], c='b')
+    plt.plot(neurons_W[:, 0], neurons_W[:, 1], 'go-', linewidth=1)
+    plt.show()
 
 for i in range(T_ORDER):
-    sys.stdout.write("Running epoch %d of %d...\r" % (i, T_ORDER))
+    sys.stdout.write("ORDER: Running epoch %d of %d...\r" % (i, T_ORDER))
     sys.stdout.flush()
 
-    learning_rate = gaussian_width(ETA_ZERO, i, TAO_SIGMA)
-    sigma_rate = gaussian_width(SIGMA_ZERO, i, TAO_SIGMA)
+    learning_rate = gaussian_width(ETA_ORDER_ZERO, i, TAO_SIGMA)
+    sigma_rate = gaussian_width(SIGMA_ORDER_ZERO, i, TAO_SIGMA)
 
     random_point_index = get_random_point_index(INPUT_SIZE)
-    winning_neuron_index= find_nearest_neuron(random_point_index, points, neurons_W)
-    for k in range(NEURON_SIZE):
-        neurons_W[k] += calc_delta_weight(learning_rate, neighbor_func(winning_neuron_index, k, sigma_rate), points[random_point_index] - neurons_W[k])
+    winning_neuron_index = find_nearest_neuron(random_point_index, points, neurons_W)
+    for j in range(NEURON_SIZE):
+        neurons_W[j] += calc_delta_weight(learning_rate, neighbor_func(winning_neuron_index, j, sigma_rate), points[random_point_index] - neurons_W[j])
 
-    if i % 20 == 0:
-        plt.clf()  # Clears plot before rendering
-        plt.scatter(points[:, 0], points[:, 1], c='b')
-        plt.scatter(neurons_W[:, 0], neurons_W[:, 1], c='g')
-        plt.pause(.2)
+    if PLOT_ION:
+        if i % 50 == 0:
+            plt.clf()  # Clears plot before rendering
+            if PLOT_POINTS:
+                plt.scatter(points[:, 0], points[:, 1], c='b', s=8)
+            plt.plot(neurons_W[:, 0], neurons_W[:, 1], 'go-', linewidth=1)
+            plt.pause(0.1)
 
-plt.pause(10)
+if PLOT_POINTS:
+    plt.scatter(points[:, 0], points[:, 1], c='b', s=4)
+plt.plot(neurons_W[:, 0], neurons_W[:, 1], 'go-', linewidth=1)
+
+print ''
+
+plt.subplot(212)
+plt.title('Convergence Phase')
+
+for i in range(T_CONV):
+    sys.stdout.write("CONV: Running epoch %d of %d...\r" % (i, T_CONV))
+    sys.stdout.flush()
+    random_point_index = get_random_point_index(INPUT_SIZE)
+    winning_neuron_index = find_nearest_neuron(random_point_index, points, neurons_W)
+    for j in range(NEURON_SIZE):
+        neurons_W[j] += calc_delta_weight(ETA_CONV, neighbor_func(winning_neuron_index, j, SIGMA_CONV), points[random_point_index] - neurons_W[j])
+
+    if PLOT_ION:
+        if i % 1000 == 0:
+            plt.clf()  # Clears plot before rendering
+            if PLOT_POINTS:
+                plt.scatter(points[:, 0], points[:, 1], c='b')
+            plt.plot(neurons_W[:, 0], neurons_W[:, 1], 'go-', linewidth=1)
+            plt.pause(0.1)
+
+if PLOT_ION:
+    plt.ioff()
+    plt.clf()
+print ''
+
+if PLOT_POINTS:
+    plt.scatter(points[:, 0], points[:, 1], c='b', s=4)
+plt.plot(neurons_W[:, 0], neurons_W[:, 1], 'go-', linewidth=1)
+plt.show()
