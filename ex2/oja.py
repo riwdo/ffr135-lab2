ef9fd5bf71f8591980db64c124bd9a30554a389a
David Söderberg <dasovm@gmail.com>
2017-10-04 13:46:30 +0200

    Added plot and code-refactor

--- ex2/oja.py
+++ ex2/oja.py
@@ -1,64 +1,76 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
-import sys
-from random import randint
+from scipy import stats
 
-learning_rate = 0.001
-#updates = 10**4
-
+LEARNING_RATE = 0.001
 updates = [0]
-for x in range(1, 20000):  # [1, 20, 40, 60, ..., 400]
+for x in range(1, 20000):
     updates.append(x)
+
 
 def generate_weights(n_points, n_neurons):
     return np.random.rand(n_points, n_neurons) * 2 - 1
 
 
 def get_input_patterns(lines):
-    patterns = []
+    a = []
     for l in lines:
         first = float(l.split("\t")[0])
         second = float(l.split("\t")[1])
-        patterns.append(np.array([first, second]).T)
-    return np.asarray(patterns)
+        a.append(np.array([first, second]).T)
+    return np.asarray(a)
 
 
 def random_pattern_index(size):
-    return np.random.randint(0,size)
+    return np.random.randint(0, size)
 
 
-def network_output(input_pattern):
-    return np.sum(np.multiply(weights,patterns[input_pattern]))
+def network_output(input_pattern, weights):
+    return np.sum(np.multiply(weights, patterns[input_pattern]))
 
 
-def update_weight(index):
-    output = network_output(index)
-    return learning_rate * output * (patterns[index]-output*weights)
+def update_weight(input_patterns, index, weights):
+    output = network_output(index, weights)
+    return LEARNING_RATE * output * (input_patterns[index] - output * weights)
 
 
-f = open('data_ex2_task2_2017.txt','r')
+def oja(input_data, weights):
+    weight_modulus = []
+    for i in range(len(updates)):
+        index = random_pattern_index(len(input_data))
+        weights += update_weight(input_data, index, weights)
+        weight_modulus.append(np.linalg.norm(weights))
+    return weights, weight_modulus
+
+
+f = open('data_ex2_task2_2017.txt', 'r')
 data_lines = f.readlines()
 f.close()
 
 patterns = get_input_patterns(data_lines)
-weights = generate_weights(1,2)
-f, axarr = plt.subplots(2,sharex=False)
-axarr[0].scatter(patterns[:,0], patterns[:,1],color='purple',s=2)
-#axarr[0].scatter(weights[:,0], weights[:,1], color='blue',s=2)
-#plt.plot(weights[:,0], weights[:,1])
-weight_modulus = []
-for i in range(len(updates)):
-    print i
-    index = random_pattern_index(len(patterns))
-    weights += update_weight(index)
-    weight_modulus.append(np.linalg.norm(weights))
+norm_patterns = stats.zscore(patterns)
 
-#print np.asarray(weight_modulus).size
-axarr[1].scatter(updates,np.asarray(weight_modulus),s=2)
-axarr[0].plot([0,weights[0,0]], [0,weights[0,1]], color='red')
+W = generate_weights(1, 2)
+norm_W = generate_weights(1, 2)
+
+W, W_mod = oja(patterns, W)
+norm_W, norm_W_mod = oja(norm_patterns, norm_W)
+
+f, axarr = plt.subplots(2, 2, sharex=False)
+axarr[0, 0].set_title('Original Data')
+axarr[0, 0].scatter(patterns[:, 0], patterns[:, 1], color='purple', s=2)
+axarr[0, 0].plot([0, W[0, 0]], [0, W[0, 1]], color='red', linewidth=4)
+
+axarr[1, 0].set_ylabel('|weights|')
+axarr[1, 0].set_xlabel('iterations')
+axarr[1, 0].scatter(updates, np.asarray(W_mod), s=2)
+
+axarr[0, 1].set_title('Normalized Data')
+axarr[0, 1].scatter(norm_patterns[:, 0], norm_patterns[:, 1], color='purple', s=2)
+axarr[0, 1].plot([0, norm_W[0, 0]], [0, norm_W[0, 1]], color='red', linewidth=4)
+
+axarr[1, 1].set_ylabel('|weights|')
+axarr[1, 1].set_xlabel('iterations')
+axarr[1, 1].scatter(updates, np.asarray(norm_W_mod), s=2)
+
 plt.show()
-
-
-
-
